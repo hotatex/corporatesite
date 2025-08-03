cab99a05a947fd09bcd344b726ca3fc0cc9fd08a
hotatex <bumpofhotate@gmail.com>
2025-08-03 13:58:17 +0900

    next.config.js を更新

--- next.config.js
+++ next.config.js
@@ -1,12 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   images: {
     unoptimized: true,
   },
-  trailingSlash: true,
-  distDir: 'out',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '' : '',
 }
 
 module.exports = nextConfig
